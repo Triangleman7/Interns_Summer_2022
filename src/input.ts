f5ef616b8e405a38464a58e962d7ceab749d0e2f
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-06-21 16:30:28 -0700

    refactor: rename filename, variable names

--- src/input.ts
+++
@@ -1,5 +0,0 @@
-function processInput() {
-    let x = document.forms["form"]["form-text-inp"].value;
-
-    console.log(x);
-}
---
+++ src/input_handler.ts
@@ -0,0 +1,5 @@
+function processInput() {
+    let textValue = document.forms["form"]["form-text-inp"].value;
+
+    console.log(textValue);
+}
