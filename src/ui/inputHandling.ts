7b22bc70b8040bf05534b76bfd2e9cccf9ba5135
Jacob Lee <JLpython@outlook.com>
2022-06-22 22:55:40 -0700

    fix: TypeScript compiler error for implicit <any> index
    Value attached to 'name' attribute used to get target <form> element
    from DOM. TS compiler expects numerical index; <any> type assertion used
    to suppress error.

--- src/ui/inputHandling.ts
+++ src/ui/inputHandling.ts
@@ -1,9 +1,10 @@
 function processInput() {
-    let textValue: string = document.forms["form"]["form-text-inp"].value;
+    let formElement: HTMLFormElement = document.forms[<any>"form"];     // Attribute 'name' value used to get <form> element from DOM (TS expects numerical index; <any> type assertion used)
+    let textValue: string = formElement["form-text-inp"].value;
 
     // Serialize text field value
     let formData: object = {textInput: textValue};
-    // Store serialized data
+    // Locally store serialized data
     localStorage.setItem("formDataJSON", JSON.stringify(formData));
 
     console.log(localStorage.getItem("formDataJSON"));
