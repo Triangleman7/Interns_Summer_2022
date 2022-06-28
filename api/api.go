69c492615817feeef36edebb51bdc7cc56ac2307
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-06-28 15:44:49 -0700

    refactor: refactor tag/attribute references according to 51f5829

--- api/api.go
+++ api/api.go
@@ -3,9 +3,11 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func ProcessInput(w http.ResponseWriter, r *http.Request) {
+func ProcessRootResponse(w http.ResponseWriter, r *http.Request) {
+	// Assert URL path directs to the root address
 	if r.URL.Path != "/" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
@@ -13,18 +15,25 @@ func ProcessInput(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "GET":
+		// Reply with root HTML document
 		http.ServeFile(w, r, "ui/index.html")
+
 	case "POST":
+		// Catch errors thrown while parsing form submission
 		err := r.ParseForm();
-		if err != nil {
-			fmt.Println(w, "ParseForm() err: %v", err)
-		}
+		if err != nil { fmt.Fprintf(w, "ParseForm() err: %v", err) }
 
-		rawTextInput := r.FormValue("form-text-inp")
-		fTextInput := CapitalizeInput(rawTextInput)
+		// Get value of text field
+		valueTextField := r.FormValue("primary-form-text")
 
-		fmt.Printf("form[name='form'] > form[name='form-text-input']:\n")
-		fmt.Printf("\tRaw: %v\n", rawTextInput)
-		fmt.Printf("\tFormatted: %v\n", fTextInput)
+		// Format value of text field
+		fvalueTextField := strings.ToUpper(valueTextField)
+
+		// Debugging
+		fmt.Printf("<form name=\"primary\">: Text Field value = \"%v\"\n", valueTextField)
+		fmt.Printf("\tFormatted: \"%v\"\n", fvalueTextField)
+
+	default:
+		fmt.Fprintf(w, "Only GET and POST requests supported")
 	}
 }
--- api/dataproc.go
+++ api/dataproc.go
@@ -1,7 +1,1 @@
 package api
-
-import (
-	"strings"
-)
-
-func CapitalizeInput(input string) string { return strings.ToUpper(input) }
