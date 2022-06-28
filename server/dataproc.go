d39a4f64ba42f439a802b3eaba2fa97bbb14fee6
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-06-28 16:32:38 -0700

    fix: error in WriteHTML() ('The system cannot find the path specified')
    If the output directory had been created previously, the program would
    catch an error indicating that the directory already existed
    (`os.IsExists()` returns `true` or equivalent value) and call
    `os.RemoveAll()` to remove the directory and all its children. In this
    case, the directory would need to be re-created, otherwise errors
    related to nonexistent paths would be raised. Now, the program performs
    a deep removal of the target directory prior to attempting to create the
    output directory.

--- server/dataproc.go
+++ server/dataproc.go
@@ -14,12 +14,14 @@ var PermissionBits os.FileMode = 0755
 func WriteHTML(filename string, value string) {
 	if filepath.Ext(filename) != ".html" {
 		fmt.Printf("Invalid HTML file name: '%v'", filename)
+		return
 	}
 
 	var path string = filepath.Join(OutputDirectory, filename)
 
+	os.RemoveAll(OutputDirectory)
 	err := os.Mkdir(OutputDirectory, PermissionBits)
-	if err != nil && os.IsExist(err) { os.RemoveAll(OutputDirectory) }
+	if err != nil { log.Fatal(err) }
 
 	err = ioutil.WriteFile(path, []byte(value), PermissionBits)
 	if err != nil { log.Fatal(err) }
