ea7b88ec445d06f03c88794d663a178ee43c4639
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-06-28 09:48:27 -0700

    revert: revert commit e21cba2

--- ui/inputHandling.ts
+++ ui/inputHandling.ts
@@ -1,18 +1,6 @@
 function processInput() {
-    let request = new XMLHttpRequest();
-    let data = new FormData()
-
     let formElement: HTMLFormElement = document.forms[<any>"form"];     // Attribute 'name' value used to get <form> element from DOM (TS expects numerical index; <any> type assertion used)
     let textValue: string = formElement["form-text-inp"].value;
 
-    data.append("text", textValue);
-
-    request.open("POST", "somewhere", true);        // Replace argument 'somwhere' with destination URL
-    request.setRequestHeader(
-        "Content-type", "applicaiton/x-www-form-urlencoded"
-    );
-    request.onload = function() {
-        console.log(this.responseText);
-    }
-    request.send(data);
+    console.log("form[name='form']", textValue);
 }
