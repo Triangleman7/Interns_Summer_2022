71ba404f3cc0bb6bcab572fc9cc937821c878443
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-06-28 10:01:33 -0700

    refactor: refactor form text value format logged to console

--- ui/inputHandling.ts
+++ ui/inputHandling.ts
@@ -2,5 +2,7 @@ function processInput() {
     let formElement: HTMLFormElement = document.forms[<any>"form"];     // Attribute 'name' value used to get <form> element from DOM (TS expects numerical index; <any> type assertion used)
     let textValue: string = formElement["form-text-inp"].value;
 
-    console.log("form[name='form']", textValue);
+    console.log(
+        `form[name='form'] > form[name='form-text-input'] = "${textValue}"`
+    );
 }
