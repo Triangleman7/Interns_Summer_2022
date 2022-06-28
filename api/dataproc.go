969d52f0406c791898060aa16b905c16ca9805a9
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-06-28 13:15:48 -0700

    fix: use uppercase capitalization instead of titlecase capitalization

--- api/dataproc.go
+++ api/dataproc.go
@@ -2,23 +2,6 @@ package api
 
 import (
 	"strings"
-	"unicode"
 )
 
-func CapitalizeInput(input string) string {
-	var res string;
-
-	for _, word := range strings.Split(input, ` `) {
-		for idx, char := range word {
-			if idx == 0 {
-				res += string(unicode.ToUpper(char))
-			} else {
-				res += string(unicode.ToLower(char))
-			}
-		}
-
-		res += string(` `)
-	}
-
-	return res
-}
+func CapitalizeInput(input string) string { return strings.ToUpper(input) }
