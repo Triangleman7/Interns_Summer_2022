429ca557152441a10217eccd83543de395405500
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-06-28 15:53:09 -0700

    refactor: update function calls according to 69c4926

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/Triangleman7/Interns_Summer_2022/api"
 )
 
-func main() {
-	http.HandleFunc("/", api.ProcessInput)
+var PORT int = 8080
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+func main() {
+	// Register handler functions
+	http.HandleFunc("/", api.ProcessRootResponse)
+
+	fmt.Printf("Listening on Localhost (Port %v)\n\n", PORT)	// Debuggin
+	err := http.ListenAndServe(fmt.Sprintf(":%v", PORT), nil)
+	if err != nil { log.Fatal(err) }
 }
