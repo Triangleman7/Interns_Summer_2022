208a9e527725898439dbd0f8432e1e56034b9c05
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-07-18 16:54:05 -0700

    doc: update in-file documentation for outputdata/html package

--- outputdata/html/fields.go
+++ outputdata/html/fields.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 )
 
+// Image replaces all instances of the format field in output identified by key with an <img>
+// element that is sourced at src. Since output is a pointer, the replacement is performed
+// in-place.
 func Image(key string, output *string, src string) {
 	var field string = fmt.Sprintf("{%v}", key)
 
@@ -14,6 +17,9 @@ func Image(key string, output *string, src string) {
 	*output = strings.ReplaceAll(*output, field, tag)
 }
 
+// Paragraph replaces all instances of the format filed in output identified by key with a <p>
+// element that has content as the innter text. Since output is a pointer, the replacement is
+// performed in-place.
 func Paragraph(key string, output *string, content string) {
 	var field string = fmt.Sprintf("{%v}", key)
 
--- outputdata/html/html.go
+++ outputdata/html/html.go
@@ -1,3 +1,4 @@
+// Package outputdata/html handles writing HTML Document (.HTML) output.
 package html
 
 import (
@@ -5,6 +6,10 @@ import (
 	"os"
 )
 
+// ReadTemplate reads the template HTML Document located at path and returns the content of the
+// HTML Document (content).
+//
+// Raises any errors encountered while reading the HTML Document.
 func ReadTemplate(path string) (content string, err error) {
 	var reader []byte
 
@@ -16,6 +21,11 @@ func ReadTemplate(path string) (content string, err error) {
 	return string(reader), nil
 }
 
+// WriteHTML writes content, the content of the output HTML Document, an HTML Document in the local
+// file system at path.
+//
+// Raises any errors encountered while writing the Word Document object contents to the target
+// file.
 func WriteHTML(path string, content string) (err error) {
 	var file *os.File
 
