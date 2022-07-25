695103c9b9263ac3b4d88a17f15d45d7fe38d9f2
Jacob Lee <JLpython@outlook.com>
2022-07-25 02:11:26 -0700

    test: update regression tests for 'makefile'

--- tests/test_makefile.py
+++ tests/test_makefile.py
@@ -4,14 +4,61 @@ Tests the commands defined in **makefile**.
 
 import os
 
+import pytest
+
 
 class TestBuild:
     """
     Tests the `$ make build` command.
     """
-    def test(self):
+    def setup(self):
         code: int = os.system("make build")
         assert code == 0
+
+    def test_out(self):
+        """
+        Tests for successful compilation of project Go package into a binary file.
+        """
+        assert os.path.exists("main.out") or os.path.exists("main.exe")
+
+    @pytest.mark.parametrize(
+        "directory",
+        ["client/styles", "server/templates/"]
+    )
+    def test_css(self, directory: str):
+        """
+        Tests for successful compilation/transpilation of SASS/SCSS files into CSS files.
+
+        :param directory: The project directory to walk
+        """
+        for root, _, files in os.walk(directory):
+            for file in files:
+                fname, fext = os.path.splitext(file)
+                if fext == ".sass" or fext == ".scss":
+                    assert os.path.exists(
+                        os.path.join(root, f"{fname}.css")
+                    ), f"No *.css file corresponding to {os.path.join(root, file)}"
+                    assert os.path.exists(
+                        os.path.join(root, f"{fname}.css.map")
+                    ), f"No *.css.map file corresponding to {os.path.join(root, file)}"
+
+    @pytest.mark.parametrize(
+        "directory",
+        ["client/styles", "server/templates/"]
+    )
+    def test_js(self, directory: str):
+        """
+        Tests for successful compilation/transpilation of TypeScript files into JavaScripts files.
+
+        :param directory: The project directory to walk
+        """
+        for root, _, files in os.walk(directory):
+            for file in files:
+                fname, fext = os.path.splitext(file)
+                if fext == ".ts":
+                    assert os.path.exists(
+                        os.path.join(root, f"{fname}.js")
+                    ), f"No *.js file corresponding to {os.path.join(root, file)}"
 
     def teardown(self):
         os.system("make clean")
