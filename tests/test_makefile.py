ab1f393085c9821b72cedca193efde916d4110e3
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-07-25 14:37:38 -0700

    test: update regression tests for 'makefile'

--- tests/test_makefile.py
+++ tests/test_makefile.py
@@ -3,8 +3,12 @@ Regression tests for the commands defined in **makefile**.
 """
 
 import os
+import subprocess
+import urllib.request
 
 import pytest
+
+from . import URL
 
 
 class TestBuild:
@@ -12,8 +16,11 @@ class TestBuild:
     Tests the `$ make build` command.
     """
     def setup(self):
-        code: int = os.system("make build")
+        code = os.system("make build")
         assert code == 0
+
+    def teardown(self):
+        os.system("make clean")
 
     def test_out(self):
         """
@@ -60,8 +67,68 @@ class TestBuild:
                         os.path.join(root, f"{fname}.js")
                     ), f"No *.js file corresponding to {os.path.join(root, file)}"
 
+
+class TestRun:
+    """
+    Tests the `$ make run` command.
+    """
+    def setup(self):
+        self.process = subprocess.Popen(["make", "run"], stdout=PIPE, stderr=PIPE)
+        self.stdout, self.stderr = process.communicate()
+
     def teardown(self):
-        os.system("make clean")
+        self.process.terminate()
+
+    def test_out(self):
+        """
+        Tests for successful compilation of project Go package into a binary file.
+        """
+        assert os.path.exists("main.out") or os.path.exists("main.exe")
+
+    @pytest.mark.parametrize(
+        "directory",
+        ["client/styles", "server/templates/"]
+    )
+    def test_css(self, directory: str):
+        """
+        Tests for successful compilation/transpilation of SASS/SCSS files into CSS files.
+
+        :param directory: The project directory to walk
+        """
+        for root, _, files in os.walk(directory):
+            for file in files:
+                fname, fext = os.path.splitext(file)
+                if fext == ".sass" or fext == ".scss":
+                    assert os.path.exists(
+                        os.path.join(root, f"{fname}.css")
+                    ), f"No *.css file corresponding to {os.path.join(root, file)}"
+                    assert os.path.exists(
+                        os.path.join(root, f"{fname}.css.map")
+                    ), f"No *.css.map file corresponding to {os.path.join(root, file)}"
+
+    @pytest.mark.parametrize(
+        "directory",
+        ["client/styles", "server/templates/"]
+    )
+    def test_js(self, directory: str):
+        """
+        Tests for successful compilation/transpilation of TypeScript files into JavaScript files.
+
+        :param directory: The project directory to walk
+        """
+        for root, _, files in os.walk(directory):
+            for file in files:
+                fname, fext = os.path.splitext(file)
+                if fext == ".ts":
+                    assert os.path.exists(
+                        os.path.join(root, f"{fname}.js")
+                    ), f"No *.js file corresponding to {os.path.join(root, file)}"
+
+    def test_localhost(self):
+        """
+        """
+        with urllib.request.urlopen(URL) as response:
+            assert response.code == 200
 
 
 class TestClean:
@@ -71,7 +138,7 @@ class TestClean:
     def setup(self):
         os.system("make build")
 
-        code: int = os.system("make clean")
+        code = os.system("make clean")
         assert code == 0
 
     def test_out(self):
