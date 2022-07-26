85111c22f9efcf70dde125f4a7f318949095cc55
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-07-26 12:44:42 -0700

    fix: repair bug in '$ python -m make clean' command

--- make/build.py
+++ make/build.py
@@ -8,10 +8,15 @@ from .constants import BINARY_NAME, SCRIPTS, STYLES, TEMPLATES
 
 
 def main():
+    # Compile/Transpile TypeScript source files
     for file in SCRIPTS.glob("*.ts"):
         os.system(f"tsc {file}")
+    
+    # Compile/Transpile SASS/SCSS source files
     os.system(f"sass {STYLES}/:{STYLES}/")
     os.system(f"sass {TEMPLATES}/:{TEMPLATES}/")
+
+    # Compile Go package
     os.system(f"go build -o {BINARY_NAME} main.go")
 
 
