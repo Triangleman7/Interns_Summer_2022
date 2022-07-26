19dd27c25600f1934cefaaf2bfc3b8953106ea62
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-07-26 12:56:27 -0700

    feat: update make package (from branch regression-tests)

--- make/__init__.py
+++ make/__init__.py
@@ -6,12 +6,3 @@ Usage:
 - `$ make run`: `python -m make run`
 - `$ make clean`: `python -m make clean`
 """
-
-from pathlib import Path
-
-
-BINARY_NAME = Path("main.out")
-
-SCRIPTS = Path("client/scripts")
-STYLES = Path("client/styles")
-TEMPLATES = Path("server/templates")
--- make/build.py
+++ make/build.py
@@ -4,14 +4,19 @@ Imitates the `$ make build` command.
 
 import os
 
-from . import BINARY_NAME, SCRIPTS, STYLES, TEMPLATES
+from .constants import BINARY_NAME, SCRIPTS, STYLES, TEMPLATES
 
 
 def main():
+    # Compile/Transpile TypeScript source files
     for file in SCRIPTS.glob("*.ts"):
         os.system(f"tsc {file}")
+
+    # Compile/Transpile SASS/SCSS source files
     os.system(f"sass {STYLES}/:{STYLES}/")
     os.system(f"sass {TEMPLATES}/:{TEMPLATES}/")
+
+    # Compile Go package
     os.system(f"go build -o {BINARY_NAME} main.go")
 
 
