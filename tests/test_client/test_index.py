d4052082ad711428caf155f362cfc8fc20c84247
EXI-Lee, Jacob J <jacob.j.lee@boeing.com>
2022-07-28 12:04:41 -0700

    test: update tests for client/index.html

--- tests/test_client/test_index.py
+++ tests/test_client/test_index.py
@@ -11,6 +11,9 @@ import requests
 from .. import URL
 
 
+HEADLESS = False
+
+
 @pytest.fixture(scope="module")
 def page(request):
     """
@@ -21,11 +24,11 @@ def page(request):
     """
     with sync_playwright() as play:
         if request.param == "chromium":
-            browser = play.chromium.launch(headless=False)
+            browser = play.chromium.launch(HEADLESS)
         elif request.param == "firefox":
-            browser = play.firefox.launch(headless=False)
+            browser = play.firefox.launch(HEADLESS)
         elif request.param == "webkit":
-            browser = play.webkit.launch(headless=False)
+            browser = play.webkit.launch(HEADLESS)
         else:
             raise ValueError(f"Could not find matching browser for {request.param}")
 
@@ -80,3 +83,33 @@ class TestIndex:
             assert href is not None
             with requests.get(href) as response:
                 assert response.status_code == 200, href
+
+    @pytest.mark.parametrize(
+        "query",
+        []
+    )
+    def test_search_form(self, page, query: str):
+        """
+        `header.main form.search`
+
+        :type page: playwright.sync_api._generated.Page
+        :param query: The search query to input into the search bar input form
+        """
+        css = "header.main form.search"
+        css_search_input = "div.input-search > input.input-text"
+
+        # Check uniqueness of `css`
+        assert len(page.query_selector_all(css)) == 1
+        element = page.query_selector(css)
+
+        # Check uniqueness of `css_search_input`
+        assert len(page.query_selector_all(css_search_input)) == 1
+        search_input = page.query_selector(css_search_input)
+
+        # Submit `query` into search form
+        before_url = page.url
+        search_input.fill(query)
+        search_input.press("Enter")
+        page.wait_for_event("submit")
+        after_url = page.url
+        assert before_url == after_url, query
