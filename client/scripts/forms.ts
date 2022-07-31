2bf3195c8284d71f85d21da0ca4d78985f40a3e6
Jacob Lee <JLpython@outlook.com>
2022-07-31 14:54:06 -0700

    refactor: refactor form handlers into classes

--- client/scripts/forms.ts
+++ client/scripts/forms.ts
@@ -1,48 +1,110 @@
 /**
- * Handles events that are triggered when the submit action of a form is invoked.
+ * Handles form submissions.
  */
 
-const formPrimary: HTMLFormElement = document.forms[<any>"primary"];
-formPrimary.addEventListener("submit", handleFormPrimary);
 
 /**
- * Handles submission of form element 'form#primary'.
- * Sends to the server a POST request containing the data from each input field.
- * 
- * Disables the default action performed by the browser on form submission.
- * 
- * @param {SubmitEvent} event   Internally passed event received on form submission invocation.
+ * Handles submit events sent to a form invoked by form submission.
  */
-function handleFormPrimary(event: SubmitEvent) {
-    let path: string = "forms/primary";
+abstract class Form {
+    id: string;
+    path: string;
+    form: HTMLFormElement;
 
-    // Disable default action
-    event.preventDefault();
+    /**
+     * Searches for the form element with its _id_ attribute set to `id`.
+     * Adds to the form element an event listener for **submit** events.
+     * Sends POST requests to the URL `path`.
+     * 
+     * @param id The value of the form element `id` attribute
+     * @param path The URL path to which POST requests are to be sent
+     */
+    constructor(id: string, path: string) {
+        this.id = id;
+        this.path = path;
+        this.form = document.forms[<any>this.id];
 
-    // Configure a POST request
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", path);
+        this.form.addEventListener(
+            "submit", this.handleRequest
+        )
+    }
 
-    // Prepare form data
-    let data = new FormData(formPrimary);
+    /**
+     * Sends to the server a POST request of the form input data.
+     * 
+     * The default actions performed by the web browser on form submission are disabled.
+     * 
+     * @param event Internally passed **submit** event received on form submission.
+     */
+    handleRequest(event: SubmitEvent): void {
+        event.preventDefault();
+    }
 
-    // Send request
-    xhr.send(data);
-
-    // Listen for 'load' event
-    xhr.onload = () => { console.log(`${path}: ${xhr.status}`); }
+    /**
+     * Handles responses sent by the server after the request is sent.
+     */
+    handleResponse(): void {}
 }
 
 
-const formSearch: HTMLFormElement = document.forms[<any>"search"];
-formSearch.addEventListener("submit", handleFormSearch);
+/**
+ * Handles form submissions to `form#primary`.
+ */
+class FormPrimary extends Form {
+    xhr: XMLHttpRequest
+
+    /**
+     * Input to `form#primary` is sent in a POST request to **\/forms/primary**.
+     */
+    constructor() {
+        super("primary", "/forms/primary");
+
+        this.xhr = new XMLHttpRequest();
+    }
+
+    /**
+     * Sends a POST request to the server containing the input to `form#primary`.
+     * 
+     * @param event Internally passed **submit** event received on form submission.
+     */
+    handleRequest(event: SubmitEvent): void {
+        // Disable default actions
+        event.preventDefault();
+
+        // Configure a POST request
+        this.xhr.open("POST", this.path);
+
+        // Prepare form data
+        let data = new FormData(this.form);
+
+        // Send request
+        this.xhr.send(data);
+
+        this.xhr.onload = () => this.handleResponse();
+    }
+
+    /**
+     * Handles responses sent by the server after the request is sent.
+     */
+    handleResponse(): void {
+        console.log(`${this.path}: ${this.xhr.response}`);
+    }
+}
+
 
 /**
- * Handles submission of form element 'form.search'.
- * 
- * Disables the default action performed by the browser on form submission.
+ * Handles form submissions to `form#search`.
  */
-function handleFormSearch(event: SubmitEvent) {
-    // Disable default action
-    event.preventDefault();
+class FormSearch extends Form {
+
+    /**
+     * Input to `form#search` is nullified.
+     */
+    constructor() {
+        super("search", "");
+    }
 }
+
+
+const formPrimary = new FormPrimary();
+const formSearch = new FormSearch();
